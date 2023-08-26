a89852de7c7e3fd5dc46f9ffecba99d809138d9e
nishal <nishalk9399@gmail.com>
2023-08-26 22:15:54 +0530

    uploading basics

--- variablespractice.sh
+++ variablespractice.sh
@@ -13,12 +13,20 @@ echo "sum of the two variables are: $c"
 # read -sp 'what do you want to print : ' Nishal
 # echo $Nishal-
 
-JUST=[bmw, benz, audi, ferrari];
+# JUST=[bmw, benz, audi, ferrari];
 
-read -a JUST
-echo "${JUST}"
+# read -a JUST
+# echo "${JUST}"
 
+#condition statemenents
 
+Name=Nishal
+
+if [ "$Name" = "Nishal" ];
+then 
+echo "the statement is correct"
+else
+echo "it is incorrect"
 
 
 
